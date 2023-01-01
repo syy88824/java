cf4ef069d6384472038c277d4afba1bc9e4cfaaf
mina0829 <116851886+mina0829@users.noreply.github.com>
2023-01-01 19:59:31 +0800

    0101update

--- Train/src/HSR.java
+++ Train/src/HSR.java
@@ -1,63 +1,93 @@
 
 public class HSR {
 	private int price;
+	private String seatType;
 	private boolean isReservedSeat;
 	
+	public HSR(int num, int departureTime, int arriveTime, String destination, int costTime, String seatType, int ticketNum){
+		super(num, departureTime, arriveTime, destination, costTime, ticketNum);
+		this.seatType = seatType;
+	}
 	
 	public int getPrice(String place,boolean isReservedSeats) {
 		if(isReservedSeat) {
 			switch(place) {
 			case "Nangang":
 				price = 40;
+				break;
 			case "Banqiao":
 				price = 40;
+				break;
 			case "Taoyuan":
 				price = 160;
+				break;
 			case "Hsinchu":
 				price = 290;
+				break;
 			case "Miaoli":
 				price = 430;
+				break;
 			case "Taichung":
 				price = 700;
+				break;
 			case "Changhua":
 				price = 820;
+				break;
 			case "Yunlin":
 				price = 930;
+				break;
 			case "Chiayi":
-				price = 1080;
+				price = 1080
+				break;
 			case "Tainan":
 				price = 1350;
+				break;
 			case "Zuoying":
 				price = 1490;
+				break;
 			}
-			}else if(!isReservedSeat) {
-				switch(place) {
-				case "Nangang":
-					price = 35;
-				case "Banqiao":
-					price = 35;
-				case "Taoyuan":
-					price = 155;
-				case "Hsinchu":
-					price = 280;
-				case "Miaoli":
-					price = 415;
-				case "Taichung":
-					price = 675;
-				case "Changhua":
-					price = 795;
-				case "Yunlin":
-					price = 900;
-				case "Chiayi":
-					price = 1045;
-				case "Tainan":
-					price = 1305;
-				case "Zuoying":
-					price = 1445;
-				
+		}else if(!isReservedSeat) {
+			switch(place) {
+			case "Nangang":
+				price = 35;
+				break;
+			case "Banqiao":
+				price = 35;
+				break;
+			case "Taoyuan":
+				price = 155;
+				break;
+			case "Hsinchu":
+				price = 280;
+				break;
+			case "Miaoli":
+				price = 415;
+				break;
+			case "Taichung":
+				price = 675;
+				break;
+			case "Changhua":
+				price = 795;
+				break;
+			case "Yunlin":
+				price = 900;
+				break;
+			case "Chiayi":
+				price = 1045;
+				break;
+			case "Tainan":
+				price = 1305;
+				break;
+			case "Zuoying":
+				price = 1445;
+				break;
 			}
 		}
 		return price;
+	}
+	
+	public String getSeatType() {
+		return seatType;
 	}
 	
 	public boolean checkSeats(String seatType) {
